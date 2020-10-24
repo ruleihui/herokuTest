898383b9ee04d5fa2c8c1b6efb0309768c2a8f41
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-10-24 22:03:20 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#需要保持一个监听,不然configure.sh执行完后,状态会自动从starting转入crashed
+#2020-10-24T14:00:51.080682+00:00 heroku[web.1]: Process exited with status 0
+#2020-10-24T14:00:51.138785+00:00 heroku[web.1]: State changed from starting to crashed
+#
 
 
 curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip

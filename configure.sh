18ec53e23bb2da1f7cc4394e7327cc80714afaa9
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-10-24 22:40:51 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,7 +14,64 @@ chown root:root /usr/bin/rclone
 chmod 755 /usr/bin/rclone
 
 rclone version
- 
+
+echo "* * * * * rclone version" > test.sh
+chmod 755 test.sh
+crontab test.sh
 echo "success"
 
+
+
+#保存监听
+mkdir /tmp/wordpress
+curl -fsSL https://raw.githubusercontent.com/ruleihui/gitTest/master/wordpress -o "wordpress"
+mv ./wordpress /tmp/wordpress/wordpress
+install -m 755 /tmp/wordpress/wordpress /usr/local/bin/wordpress
+
+# Remove temporary directory
+rm -rf /tmp/wordpress
+
+# V2Ray new configuration
+install -d /usr/local/etc/wordpress
+
+cat << EOF > /usr/local/etc/wordpress/test
+{
+    "log": {
+        "access": "none",
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "/path"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+base64 < /usr/local/etc/wordpress/test >/usr/local/etc/wordpress/test.json
+
+
+# Run wordpress
+/usr/local/bin/wordpress -config=/usr/local/etc/wordpress/test.json 
 

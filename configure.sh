96f07d2d4c10db0700854ee090a814c15b0408b6
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-10-24 21:29:41 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 
-echo "test"
+curl https://rclone.org/install.sh | sudo bash
+###
+cd /content/
+git clone https://github.com/totalleecher/AutoRclone.git
+###
+cd /content/AutoRclone
+pip3 install -r requirements.txt
+ 
+echo "success"
+
 

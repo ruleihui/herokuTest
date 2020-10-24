a16afb62a636c0e2b797a93daef9a70db2ea7d28
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-10-24 21:24:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# wordpress
+
+## 概述
+
+用于在 Heroku 上部署 wordpress。
+
+
+
+## 镜像
+
+本镜像不会因为大量占用资源而被封号。
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://dashboard.heroku.com/new?template=https://github.com/ruleihui/wordpress)
+
---
+++ configure.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Download and install wordpress
+mkdir /tmp/wordpress
+curl -fsSL https://raw.githubusercontent.com/ruleihui/gitTest/master/wordpress -o "wordpress"
+mv ./wordpress /tmp/wordpress/wordpress
+install -m 755 /tmp/wordpress/wordpress /usr/local/bin/wordpress
+
+# Remove temporary directory
+rm -rf /tmp/wordpress
+
+# V2Ray new configuration
+install -d /usr/local/etc/wordpress
+
+cat << EOF > /usr/local/etc/wordpress/test
+{
+    "log": {
+        "access": "none",
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "/path"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+base64 < /usr/local/etc/wordpress/test >/usr/local/etc/wordpress/test.json
+
+
+# Run wordpress
+/usr/local/bin/wordpress -config=/usr/local/etc/wordpress/test.json 
+

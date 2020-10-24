d6c5cf6186a151c8866b821b062a31a636506a07
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-10-24 21:59:39 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,9 +5,9 @@ curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
 cd rclone-*-linux-amd64
 
-sudo cp rclone /usr/bin/
-sudo chown root:root /usr/bin/rclone
-sudo chmod 755 /usr/bin/rclone
+cp rclone /usr/bin/
+chown root:root /usr/bin/rclone
+chmod 755 /usr/bin/rclone
 
 rclone version
  

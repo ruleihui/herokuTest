568429632450c3377c6ea75a2fe0d704b0a02e0e
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-10-28 13:10:44 +0800

    Update shell1.sh

--- shell1.sh
+++ shell1.sh
@@ -28,6 +28,7 @@ mkdir accounts
 fclone copy google:accounts.zip /rcloneTemp/accounts/
 unzip -q -o -j "/rcloneTemp/accounts/accounts.zip" -d "accounts/"
 
+echo "accounts file get and unzip over" 
 
 fclone copy kkk:{10p6gvNkHhFmhGIsCIV7LYJImyzTq7OsD} kkk:{0AFLoarsGwGcIUk9PVA}baidu --drive-server-side-across-configs --stats=3s --stats-one-line  -vP  --transfers=300 --checkers=128 --drive-pacer-min-sleep=1ms --check-first --ignore-existing &
 echo $!>currentPid

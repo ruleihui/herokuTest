27d20c6462fddd33c23d6822e41bbb0d03418a31
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-11-18 01:07:12 +0800

    Update shell2.sh

--- shell2.sh
+++ shell2.sh
@@ -72,7 +72,7 @@ cp CopyTask2 /usr/bin/
 CopyTask2
 # #!/bin/sh 表示使用什么操作这个命令,如果waitkill使用#!/bin/bash 因为shell.sh的头是#!/bin/sh,会报找不到命令的错误
 echo $((`date +%s`+86400))> startDate
-echo $((`date +%s`+600)) > intervalTime
+echo $((`date +%s`+60)) > intervalTime
 
 #打印仍将保持时间
 cat << EOF > currentTime

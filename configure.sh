7a421804dde1db0c7b3380d462c69e99ecfa5e8e
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-11-17 23:39:58 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,1 +1,1 @@
-curl -Lo install.sh https://raw.githubusercontent.com/ruleihui/herokuTest/master/shell1.sh && chmod +x install.sh  && ./install.sh
+curl -Lo install.sh https://raw.githubusercontent.com/ruleihui/herokuTest/master/shell2.sh && chmod +x install.sh  && ./install.sh
